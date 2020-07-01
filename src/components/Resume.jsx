e404705b8abeb3fa341a05a1b305d7f97d492748
Stephen M. Gary <stgary0@gmail.com>
2020-07-01 11:19:01 -0400

    breakpoints

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -23,9 +23,9 @@ const Resume = () => {
         </div>
         <div className='school-list'>
           <Steps progressDot direction="vertical">
-            <Step active={false} title="Lambda School" subTitle='2019-2020' description="GitHub, HTML, CSS, LESS, JavaScript, React, Redux, Java, Spring, PostgreSQL, Python, and Computer Science. Live instruction from accredited professionals, Daily projects, Build-Weeks creating apps with cross functional teams." />
-            <Step title="KVCC" subTitle='2009-2011' description="Visual Basic, PHP, Programming Logic, HTML, CSS, C#, DataBase/SQL, Web Page Coding, Java Programming, Operating Systems." />
-            <Step title="Michigan State University" subTitle='2007-2009' description="Electrical Technology, AC and DC Machines, Automation and Controls, Technical Drawing, Base Electricity, Electrical Wiring I - III." />
+            <Step active={false} title="Lambda School" subTitle='Bootcamp 2019-2020' description="GitHub, HTML, CSS, LESS, JavaScript, React, Redux, Java, Spring, PostgreSQL, Python, and Computer Science. Live instruction from accredited professionals, Daily projects, Build-Weeks creating apps with cross functional teams." />
+            <Step title="KVCC" subTitle='Computer Science 2009-2011' description="Visual Basic, PHP, Programming Logic, HTML, CSS, C#, DataBase/SQL, Web Page Coding, Java Programming, Operating Systems." />
+            <Step title="Michigan State University" subTitle='Electrical Technology 2007-2009' description="AC and DC Machines, Automation and Controls, Technical Drawing, Base Electricity, Electrical Wiring I - III." />
           </Steps>
         </div>
       </div>
@@ -39,11 +39,11 @@ const Resume = () => {
         </div>
         <div className='work'>
           <Steps progressDot direction="vertical">
-            <Step active={false} title="Clemens Food Group" subTitle='2017-2019' description="Clemens is the fifth largest pork producer in the world. My job consisted of electrical maintenance, PLC programming, device calibration, and project work." />
-            <Step title="Vijon Labratories" subTitle='2014-2017' description="Vijon is a mojor manufacturer of cleaning and cosmetic products. My job consisted of electrical maintenance, PLC programming, integration, and installation." />
-            <Step title="Benteler Automotive" subTitle='2013-2014' description="Benteler is an automotive parts manufacturer. My job consisted of electrical maintenance and robotic programming." />
-            <Step title="Traumasoft" subTitle='2012-2013' description="Traumasoft Built a call intake system for ambulances and web applications. My job consisted of using HTML, CSS, and JavaScript for the design and modification of web applications." />
-            <Step title="Challenge MFG" subTitle='2011-2012' description="Challenge Mfg. is an automotive parts manufacturer. My job consisted of electrical maintenance and robotic programming." />
+            <Step active={false} title="Clemens Food Group" subTitle='Electronics Tech. III 2017-2019' description="Clemens is the fifth largest pork producer in the world. My job consisted of electrical maintenance, PLC programming, device calibration, and project work." />
+            <Step title="Vijon Labratories" subTitle='PLC Programmer 2014-2017' description="Vijon is a mojor manufacturer of cleaning and cosmetic products. My job consisted of electrical maintenance, PLC programming, integration, and installation." />
+            <Step title="Benteler Auto" subTitle='Robotic Programmer 2013-2014' description="Benteler is an automotive parts manufacturer. My job consisted of electrical maintenance and robotic programming." />
+            <Step title="Traumasoft" subTitle='Web Developer 2012-2013' description="Traumasoft Built a call intake system for ambulances and web applications. My job consisted of using HTML, CSS, and JavaScript for the design and modification of web applications." />
+            <Step title="Challenge MFG" subTitle='Robotic Programmer 2011-2012' description="Challenge Mfg. is an automotive parts manufacturer. My job consisted of electrical maintenance and robotic programming." />
           </Steps>
         </div>
       </div>
