62210369da6e49c256b00f84bb2c4767c966a6de
Stephen M. Gary <stgary0@gmail.com>
2020-07-02 20:18:30 -0400

    switched drawer to the right

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import { Drawer } from 'antd';
 import Logo from '../img/logo.png';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { 
+  faBars, 
+  faHome,
+  faAddressCard,
+  faPaintRoller,
+  faFile,
+  faPaperPlane,
+  faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Menu() {
 
   const [visible, setVisible] = useState(false);
   
-  const img = <img className='menu-logo' src={Logo} alt='logo' /> 
+  const img = <img className='menu-logo' src={Logo} alt='logo' />;
 
   const showDrawer = () => {
     setVisible(true);
@@ -18,21 +25,37 @@ export default function Menu() {
     setVisible(false);
   };
 
+
   return (
   <div className='drawer-container'>
     <FontAwesomeIcon onClick={showDrawer} className='menu-button' icon={faBars} />
     <Drawer
       title={img}
-      placement="top"
+      placement="right"
       closable={false}
       onClose={onClose}
       visible={visible}
-    >
-      <a href='#home'>Home</a>
-      <a href='#about'>About</a>
-      <a href='#resume'>Resume</a>
-      <a href='#portfolio'>Portfolio</a>
-      <a href='#contact'>Contact</a>
+    > 
+      <a onClick={onClose} href='#home'>
+      <FontAwesomeIcon className='drawer-icon' icon={faHome} />  
+      <span>Home</span>
+      </a>
+      <a onClick={onClose} href='#about'>
+      <FontAwesomeIcon className='drawer-icon' icon={faAddressCard} /> 
+        <span>About</span>
+      </a>
+      <a onClick={onClose} href='#resume'>
+      <FontAwesomeIcon className='drawer-icon' icon={faFile} /> 
+        <span>Resume</span>
+        </a>
+      <a onClick={onClose} href='#portfolio'>
+      <FontAwesomeIcon className='drawer-icon' icon={faPaintRoller} /> 
+        <span>Portfolio</span>
+      </a>
+      <a onClick={onClose} href='#contact'>
+        <FontAwesomeIcon className='drawer-icon' icon={faPaperPlane} /> 
+        <span>Contact</span>
+      </a>
     </Drawer>
   </div>
   );
